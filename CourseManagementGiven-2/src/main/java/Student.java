be8e8fd60cdc13e5ef3fd4e47df477b27a4ed22c
armandominor29 <armandominor@armandos-air.lan>
2019-03-30 11:09:53 -0700

    Implementing if statement to consider all cases

--- CourseManagementGiven-2/src/main/java/Student.java
+++ CourseManagementGiven-2/src/main/java/Student.java
@@ -1,11 +1,21 @@
 package main.java;
 
+/*
+File:	Student.java
+Author:	Armando Minor (aminor1)
+Date:	03-29-19
+
+Description: java file for defining students.
+*/
+
 import java.awt.List;
 import java.util.ArrayList;
 
 /**
- * This is student class encapsulating all the details for a student.
- */
+Class:	Student
+
+Description: defines student values: asurite, major, overall_grade, course, Courses2
+*/
 public class Student {
     private String asurite;
     private Major major;
@@ -13,6 +23,13 @@ public class Student {
     private ArrayList<Course> courses = new ArrayList<Course>();
     private ArrayList<Course> Courses2 = null;
     
+    /**
+    Method: Student
+    Inputs: asurite, major
+    Returns: n/a (setter)
+
+    Description: sets asurite,major value and calls setOverall_grade method to set initial grade to 0
+  */
     public Student(String asurite, Major major){
         this.setAsurite(asurite);
         this.setMajor(major);
@@ -23,33 +40,37 @@ public class Student {
     public String getAsurite() {
          return asurite;
     }
-
+    
     public void setAsurite(String asurite ) {
         this.asurite = asurite;
     }
-
+    
     public Major getMajor() {
         return major;
     }
     
-    public boolean Register_forCourse(Course course) {
+    /**
+    Method: Register_forCourse
+    Inputs: course
+    Returns: course boolean
+
+    Description: registers student to specific course
+  */
+    public boolean register_forCourse(Course course) {
       course.addStudent(this);
       return courses.add(course);
   }
-
+    
     public void setMajor(Major major) {
         this.major = major;
     }
-
+    
     public double getOverall_grade(){
         return overall_grade;
     }
-
+    
     public void setOverall_grade(double overall_grade) {
         this.overall_grade = overall_grade;
     }
     
-    
-
-
 }
